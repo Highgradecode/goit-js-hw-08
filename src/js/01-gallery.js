445c450ae9d84999d1c864605c0866f6e4ef6929
Victoria <92218796+Highgradecode@users.noreply.github.com>
2022-11-29 14:21:52 +0200

    Image gallery - done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryLayout = galleryItems.map(({preview, original, description}) => {
+    return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+}).join('')
+
+galleryContainer.insertAdjacentHTML("afterbegin", galleryLayout);
+
+
+const lightboxOptions = {
+    captionsData: 'alt',
+    captionDelay: 250,
+}
+
+var lightbox = new SimpleLightbox('.gallery a', lightboxOptions);
