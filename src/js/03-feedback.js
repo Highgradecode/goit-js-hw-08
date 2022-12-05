6d4f4ad957a9376825a1a93161b21658188e0126
Victoria <92218796+Highgradecode@users.noreply.github.com>
2022-12-05 12:15:13 +0200

    feedback-form-done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+
+
+form.addEventListener('input', throttle( () => {
+    const inputSettings = {
+        email: form.email.value,
+        message: form.message.value,
+    }
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(inputSettings))
+}, 500));
+
+
+
+
+const savedFormInput = localStorage.getItem('feedback-form-state')
+const parsedFormInput = JSON.parse(savedFormInput)
+
+if (parsedFormInput) {
+    form.email.value = parsedFormInput.email;
+    form.message.value = parsedFormInput.message;
+};
+
+// // clear after submit input
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log({
+        email: form.email.value,
+        message: form.message.value,
+    });
+    localStorage.removeItem('feedback-form-state')
+    form.reset()
+})
